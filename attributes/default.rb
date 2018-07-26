68b550043c9597f3b16b5d49207ea0d03f41e167
Ariel Cardoso <arielccardoso@live.com>
2018-07-26 02:55:12 -0300

    Ajustes e finalização do cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,32 +2,11 @@
 # Cookbook Name:: zabbix-agent
 # Attributes:: default
 
-# Directories
-#default['zabbix']['etc_dir'] = case node['platform_family']
-#                               when 'windows'
-#                                 ::File.join(ENV['PROGRAMDATA'], 'zabbix')
-#                               else
-#                                 '/etc/zabbix'
-#                               end
-#default['zabbix']['agent']['include_dir']            = ::File.join(node['zabbix']['etc_dir'], 'zabbix_agentd.d')
-#default['zabbix']['agent']['config_file']            = ::File.join(node['zabbix']['etc_dir'], 'zabbix_agentd.conf')
-#default['zabbix']['agent']['userparams_config_file'] = ::File.join(node['zabbix']['agent']['include_dir'], 'user_params.conf')
-#default['zabbix']['agent']['pid_file']               = '/var/run/zabbix/zabbix_agentd.pid'
-#if node['platform'] == 'windows'
-#  default['zabbix']['install_dir']      = node['zabbix']['etc_dir']
-#  default['zabbix']['log_dir']          = ::File.join(node['zabbix']['etc_dir'], 'log')
-#  default['zabbix']['agent']['scripts'] = ::File.join(node['zabbix']['etc_dir'], 'scripts')
-#else
-#  default['zabbix']['install_dir']      = '/opt/zabbix'
-#  default['zabbix']['lock_dir']         = '/var/lock/subsys'
-#  default['zabbix']['log_dir']          = '/var/log/zabbix'
-#  default['zabbix']['run_dir']          = '/var/run/zabbix'
-#  default['zabbix']['agent']['scripts'] = '/etc/zabbix/scripts'
-#end
-
 # package conf
-default['zabbix']['agent']['version'] = '3.4'
-default['zabbix']['agent']['config_file'] = '/etc/zabbix/zabbix_agentd.conf'
+default['zabbix']['agent']['version'] = '1:3.4.11-1+xenial'
+default['zabbix']['agent']['conf_dir'] = '/etc/zabbix'
+default['zabbix']['agent']['config_file'] = ::File.join(node['zabbix']['agent']['conf_dir'],'zabbix_agentd.conf')
+default['zabbix']['agent']['user'] = 'root'
 
 # zabbix-server
 default['zabbix']['server']['ip'] = '10.124.26.5'
@@ -35,16 +14,16 @@ default['zabbix']['server']['ip'] = '10.124.26.5'
 # repo package install
 case node['platform']
 when 'ubuntu', 'debian'
-  default['zabbix']['agent']['package']['repo_uri'] = "http://repo.zabbix.com/zabbix/3.0/#{node['platform']}/"
+  default['zabbix']['agent']['package']['repo_uri'] = 'http://repo.zabbix.com/zabbix/3.4/ubuntu'
   default['zabbix']['agent']['package']['repo_key'] = 'http://repo.zabbix.com/zabbix-official-repo.key'
 when 'redhat', 'centos', 'scientific', 'oracle'
-  default['zabbix']['agent']['package']['repo_uri'] = 'http://repo.zabbix.com/zabbix/3.0/rhel/$releasever/$basearch/'
+  default['zabbix']['agent']['package']['repo_uri'] = 'http://repo.zabbix.com/zabbix/3.4/rhel/$releasever/$basearch/'
   default['zabbix']['agent']['package']['repo_key'] = 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX'
 when 'amazon'
   # current Amazon AMI based on RHEL6 at the moment
-  default['zabbix']['agent']['package']['repo_uri'] = 'http://repo.zabbix.com/zabbix/3.0/rhel/6/$basearch/'
+  default['zabbix']['agent']['package']['repo_uri'] = 'http://repo.zabbix.com/zabbix/3.4/rhel/6/$basearch/'
   default['zabbix']['agent']['package']['repo_key'] = 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX'
 when 'fedora'
-  default['zabbix']['agent']['package']['repo_uri'] = 'http://repo.zabbix.com/zabbix/3.0/rhel/7/$basearch/'
+  default['zabbix']['agent']['package']['repo_uri'] = 'http://repo.zabbix.com/zabbix/3.4/rhel/7/$basearch/'
   default['zabbix']['agent']['package']['repo_key'] = 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX'
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,4 +4,94 @@
 #
 # Copyright:: 2018, Ariel Cardoso, All Rights Reserved.
 
-include_recipe 'zabbix-agent::install'
+#include_recipe 'build-essential'
+#include_recipe 'zabbix-agent::install'
+
+case node['platform']
+when 'windows'
+  include_recipe 'chocolatey'
+  chocolatey 'zabbix-agent'
+when 'ubuntu', 'debian'
+  apt_repository 'zabbix' do
+    uri node['zabbix']['agent']['package']['repo_uri']
+    distribution node['lsb']['codename']
+    components ['main']
+    key node['zabbix']['agent']['package']['repo_key']
+  end
+when 'redhat', 'centos', 'scientific', 'oracle', 'amazon', 'fedora'
+  yum_repository 'zabbix' do
+    repositoryid 'zabbix'
+    description 'Zabbix Official Repository'
+    baseurl node['zabbix']['agent']['package']['repo_uri']
+    gpgkey node['zabbix']['agent']['package']['repo_key']
+    sslverify false
+    action :create
+  end
+  yum_repository 'zabbix-non-supported' do
+    repositoryid 'zabbix-non-supported'
+    description 'Zabbix Official Repository non-supported - $basearch'
+    baseurl node['zabbix']['agent']['package']['repo_uri']
+    gpgkey node['zabbix']['agent']['package']['repo_key']
+    sslverify false
+    action :create
+  end
+end
+
+package 'zabbix-agent' do
+  version node['zabbix']['agent']['version']
+  if node['platform_family'] == 'debian'
+    options '-o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"'
+  end
+  action :install
+end
+
+execute 'enable_service' do
+  command   'systemctl enable zabbix-agent.service'
+  group     node['zabbix']['agent']['user']
+  user      node['zabbix']['agent']['user']
+  action    :nothing
+end
+
+execute 'umask_service' do
+  command   'systemctl unmask zabbix-agent.service'
+  group     node['zabbix']['agent']['user']
+  user      node['zabbix']['agent']['user']
+  action    :nothing
+  notifies :run, 'execute[enable_service]', :immediately
+end
+
+execute 'daemon_reload' do
+  command   'systemctl daemon-reload'
+  group     node['zabbix']['agent']['user']
+  user      node['zabbix']['agent']['user']
+  action    :nothing
+  notifies :run, 'execute[umask_service]', :immediately
+end
+
+template 'zabbix-service' do
+  path '/etc/systemd/system/zabbix-agent.service'
+  source 'zabbix-agent.service.erb'
+  owner node['zabbix']['agent']['user']
+  group node['zabbix']['agent']['user']
+  mode '0700'
+  action :create
+  notifies :run, 'execute[daemon_reload]', :immediately
+end
+
+template 'zabbix-conf' do
+  path node['zabbix']['agent']['config_file']
+  source 'zabbix_agentd.erb'
+  owner node['zabbix']['agent']['user']
+  group node['zabbix']['agent']['user']
+  mode '0700'
+  variables(
+    zabbixServer: node['zabbix']['server']['ip'])
+  action :create
+  #notifies :run, 'template[zabbix-conf]', :immediately
+end
+
+service 'zabbix-agent' do
+  pattern 'zabbix-agent.service'
+  #restart_command ''
+  subscribes :restart, 'template[zabbix-conf]', :immediately
+end
